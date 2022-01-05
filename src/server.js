0b802c69a54946f4d96a399dedb57b6345533d79
Saad Jutt <ihsan.distranger@gmail.com>
2022-01-05 22:15:47 +0500

    feat: new routes added for todos

--- src/server.js
+++ src/server.js
@@ -1,24 +1,85 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
+app.use(express.json())
 
-app.get('/todo', function (req, res) {
-  res.send('Rendering All Todosiiiiii')
-});
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-app.post('/todo', function (req, res) {
-  res.send('Creating a new todo task')
-});
+const todo1 = {
+  id: 0,
+  value: 'default1',
+}
 
-app.patch('/todo/:id', function (req, res) {
-  res.send('updating the todo task')
-});
+const todo2 = {
+  id: 1,
+  value: 'default2',
+}
 
-app.delete('/todo/:id', function (req, res) {
-  res.send('deleting the task')
-});
+const todos = [todo1, todo2]
 
-app.listen(3000);
+app.get('/todos', function (req, res) {
+  res.send(todos)
+})
+
+app.get('/todos/:id', function (req, res) {
+  const { id } = req.params // object destructuring
+
+  if (!id) return res.status(400).send('Id not present')
+
+  const foundTodo = todos.find((t) => t.id == id)
+  if (!foundTodo) return res.status(400).send(`No Todo found against id: ${id}`)
+
+  return res.send(foundTodo)
+})
+
+app.post('/todos', function (req, res) {
+  let value
+  if (req.body && req.body.value) value = req.body.value
+
+  if (!value) return res.status(400).send('value is not present')
+
+  const newTodo = {
+    id: todos.length,
+    value,
+  }
+
+  todos.push(newTodo)
+
+  res.send(newTodo)
+})
+
+app.patch('/todos/:id', function (req, res) {
+  const { id } = req.params // object destructuring
+
+  if (!id) return res.status(400).send('Id not present')
+
+  const foundTodo = todos.find((t) => t.id == id)
+  if (!foundTodo) return res.status(400).send(`No Todo found against id: ${id}`)
+
+  let value
+  if (req.body && req.body.value) value = req.body.value
+
+  if (!value) return res.status(400).send('value is not present')
+
+  foundTodo.value = value
+
+  res.send(foundTodo)
+})
+
+app.delete('/todos/:id', function (req, res) {
+  const { id } = req.params // object destructuring
+
+  if (!id) return res.status(400).send('Id not present')
+
+  const foundTodoIndex = todos.findIndex((t) => t.id == id)
+  if (foundTodoIndex == -1)
+    return res.status(400).send(`No Todo found against id: ${id}`)
+
+  todos.splice(foundTodoIndex, 1)
+
+  res.send('task is deleted')
+})
+
+app.listen(3000)
